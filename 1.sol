2ff526885d103fe3d3827e60851344194f616dbb
Gearhead090 <114571050+Gearhead090@users.noreply.github.com>
2023-06-04 22:53:43 +0530

    Update 1.sol

--- 1.sol
+++ 1.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Clement is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Clement", "CDS") {}
+contract madmax is ERC20 {
+    address owner;
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    constructor() ERC20("clement", "CLEM") {
+        owner = msg.sender;
+        _mint(owner, 500 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public  {
+        require(msg.sender == owner, "Only owner can mint");
         _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+        
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
